decc74dd4a0711b382293150379ec63369010a14
Franco Ponticelli <franco.ponticelli@gmail.com>
2025-05-02 22:11:41 -0600

    fix: update module path exports and enhance configuration for alias resolution

--- packages/tml-parser/vite.config.ts
+++ packages/tml-parser/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
 
 export default defineConfig({
   build: {
@@ -15,4 +16,9 @@ export default defineConfig({
     },
   },
   plugins: [dts()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 })
